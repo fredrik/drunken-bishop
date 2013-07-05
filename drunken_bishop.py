b32fa24db8660b09ded7d969d27dcbc4033b9d47
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2013-07-05 14:54:06 +0100

    implement move logic

--- drunken_bishop.py
+++ drunken_bishop.py
@@ -1,13 +1,15 @@
 # coding: utf-8
 import itertools
+from collections import Counter
 
 """
-
-
-
 The bishop wakes up in the center of a room. He is drunk and stumbles around,
-putting down coins at each position he passes. The fingerprint determines his
-steps.
+putting down coins at each position he passes. The bishop only walks diagonally
+much like bishops normally found on chess boards. The fingerprint determines
+his steps.
+
+The room is 17 positions wide and 9 positions long. The bishop starts in the
+center of the room.
 """
 
 """
@@ -19,8 +21,12 @@ Because the bishop can only move one of four valid ways, we can represent this i
 “11″ means our bishop takes one move diagonally to the south-east.
 """
 
+# keep track of where the coins are at
+coins = Counter()
+
 # the bishop starts in the center of the room
-STARTING_POSITION = 76
+STARTING_POSITION = (8, 4)
+ROOM_DIMENSIONS = (17, 9)
 
 
 def hex_byte_to_binary(hex_byte):
@@ -57,6 +63,7 @@ class Direction(object):
         self.dx = dx
         self.dy = dy
 
+
 NW = Direction(dx=-1, dy=-1)
 NE = Direction(dx=1, dy=-1)
 SW = Direction(dx=-1, dy=1)
@@ -69,21 +76,38 @@ direction_lookup = {
     '11': SE,
 }
 
-def steps_from_fingerprint(fingerprint):
+def directions_from_fingerprint(fingerprint):
     """
     Convert the fingerprint (16 hex-encoded bytes separated by colons)
     to steps (one of four directions: NW, NE, SW, SE).
     """
-    position = STARTING_POSITION
     for hex_byte in fingerprint.split(':'):
         binary = hex_byte_to_binary(hex_byte)
         # read each bit-pair in each word right-to-left (little endian)
         for bit_pair in bit_pairs(binary):
             direction = direction_lookup[bit_pair]
-            # ..
+            yield direction
 
+
+MAX_X = ROOM_DIMENSIONS[0] -1
+MAX_Y = ROOM_DIMENSIONS[1] -1
+
+def move(position, direction):
+    """
+    Returns new position given current position and direction to move in.
+    """
+    x, y = position
+    assert 0 <= x <= MAX_X
+    assert 0 <= y <= MAX_Y
+    nx, ny = x + direction.dx, y + direction.dy
+    # the drunk bishop is hindered by the wall.
+    nx = 0 if nx <= 0 else min(nx, MAX_X)
+    ny = 0 if ny <= 0 else min(ny, MAX_Y)
+    return nx, ny
 
 
 def drunken_bishop(fingerprint):
-    for step in steps_from_fingerprint(fingerprint):
-        pass
+    position = STARTING_POSITION
+    for direction in directions_from_fingerprint(fingerprint):
+        position = move(position, direction)
+        coins[position] += 1  # drop coin
--- test_peripherals.py
+++ test_peripherals.py
@@ -1,5 +1,10 @@
+import pytest
+
 from drunken_bishop import hex_byte_to_binary
 from drunken_bishop import bit_pairs
+from drunken_bishop import move
+from drunken_bishop import Direction
+from drunken_bishop import NW, NE, SW, SE
 
 
 def test_hex_byte_to_binary():
@@ -13,3 +18,36 @@ def test_hex_byte_to_binary():
 def test_bit_pairs():
     assert bit_pairs('10101100') == ['00', '11', '10', '10']
     assert bit_pairs('00011011') == ['11', '10', '01', '00']
+
+
+def test_move_refuses_bad_position():
+    any_direction = Direction(dx=0, dy=1)
+    with pytest.raises(AssertionError):
+        move((-1, 0), any_direction)
+    with pytest.raises(AssertionError):
+        move((200, 0), any_direction)
+    with pytest.raises(AssertionError):
+        move((0, -10), any_direction)
+    with pytest.raises(AssertionError):
+        move((0, 300), any_direction)
+
+def test_move_in_corners():
+    assert move((0, 0), NW) == (0, 0)
+    assert move((16, 0), NE) == (16, 0)
+    assert move((0, 8), SW) == (0, 8)
+    assert move((16, 8), SE) == (16, 8)
+
+def test_move():
+    assert move((8, 4), NW) == (7, 3)
+    assert move((8, 4), NE) == (9, 3)
+    assert move((8, 4), SW) == (7, 5)
+    assert move((8, 4), SE) == (9, 5)
+
+def test_move_slides_along_the_wall():
+    left_side = (0, 1)
+    assert move(left_side, NW) == (0, 0)
+    assert move(left_side, SW) == (0, 2)
+
+    left_side = (16, 1)
+    assert move(left_side, NE) == (16, 0)
+    assert move(left_side, SE) == (16, 2)
